09f7bc8d7a33fb0d22290cf9c6c8ca311e2b7211
ayb1337 <aero.glowsky@gmail.com>
2023-07-23 00:59:24 +0100

    add putchar in a separate file

--- _printf.c
+++ _printf.c
@@ -1,15 +1,7 @@
 #include <unistd.h>
 #include <stdarg.h>
 #include "main.h"
-/**
- * _putchar - write a char to stdout
- * @c: The character to print
- * Return: The number of characters written
- */
-int _putchar(char c)
-{
-return (write(1, &c, 1));
-}
+
 /**
  * _print_char - print a char
  * @args: The argument list
