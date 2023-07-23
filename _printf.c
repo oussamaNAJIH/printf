d5dbd01c881b41c98fe6b3297c022ac2f0969d5e
oussamaNAJIH <oussamanajih6@gmail.com>
2023-07-23 11:23:28 -0700

    printf

--- _printf.c
+++ _printf.c
@@ -1,6 +1,15 @@
-#include <unistd.h>
 #include <stdarg.h>
-#include "main.h"
+#include <unistd.h>
+#include <stdio.h>
+
+
+
+/**
+ * _printf - custom printf function
+ * @format: The format string containing conversion specifiers
+ * Return: The number of characters printed (excluding the null byte)
+ */
+int _printf(const char *format, ...);
 /**
  * _putchar - write a char to stdout
  * @c: The character to print
@@ -10,76 +19,28 @@ int _putchar(char c)
 {
 return (write(1, &c, 1));
 }
-/**
- * _print_char - print a char
- * @args: The argument list
- * Return: The number of characters written
- */
-int _print_char(va_list args)
-{
-char ch = va_arg(args, int);
-_putchar(ch);
-return (1);
-}
+
+
 /**
  * _print_string - print a %s string
- * @args: The argument list
+ * @str: The string to print
  * Return: The number of characters written
  */
-int _print_string(va_list args)
+int _print_string(char *str)
 {
-int count = 0;
-char *str = va_arg(args, char *);
+int i = 0;
 if (str == NULL)
-str = "(null)";
-while (*str)
 {
-_putchar(*str);
-str++;
-count++;
+_print_string("(null)");
+return (0);
 }
-return (count);
-}
-/**
- * _print_integer - print a signed integer (d, i)
- * @args: The argument list
- * Return: The number of characters written
- */
-int _print_integer(va_list args)
+while (str[i])
 {
-int num = va_arg(args, int);
-int count = 0;
-int divisor = 1;
-int temp;
-int sign = 1;
-if (num < 0)
-{
-sign = -1;
-count += _putchar('-');
-num = -num;
+_putchar(str[i]);
+i++;
 }
-if (num == 0)
-{
-count += _putchar('0');
-return (count);
+return (i);
 }
-temp = num;
-while (temp > 9)
-{
-divisor *= 10;
-temp /= 10;
-}
-while (divisor > 0)
-{
-int digit = num / divisor;
-count += _putchar('0' + digit * sign);
-num %= divisor;
-divisor /= 10;
-}
-return (count);
-}
-
-
 /**
  * _printf - custom printf function
  * @format: The format string containing conversion specifiers
@@ -87,41 +48,54 @@ return (count);
  */
 int _printf(const char *format, ...)
 {
-va_list args;
 int count = 0;
+va_list args;
 va_start(args, format);
+if (!format || !format[0])
+{
+return (-1);
+}
 while (*format)
 {
-if (*format != '%')
+if (*format == '%')
 {
-count += _putchar(*format);
+format++;
+if (*format == 'c')
+{
+char c = va_arg(args, int);
+_putchar(c);
+count += _putchar(c);
+}
+else if
+(*format == 's')
+{
+char *str = va_arg(args, char *);
+count += _print_string(str);
+}
+else if
+(*format == '%')
+{
+_putchar('%');
+count++;
 }
 else
 {
-format++;
-switch (*format)
+_putchar('%');
+count++;
+if (*format)
 {
-case 'c':
-count += _print_char(args);
-break;
-case 's':
-count += _print_string(args);
-break;
-case 'd':
-case 'i':
-count += _print_integer(args);
-break;
-case '%':
-count += _putchar('%');
-break;
-default:
-count += _putchar('%');
-count += _putchar(*format);
-break;
+_putchar(*format);
+count++;
 }
 }
 format++;
 }
-va_end(args);
+else
+{
+_putchar(*format);
+format++;
+count++;
+}
+}
 return (count);
 }
