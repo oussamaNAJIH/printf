8a71c7941667481f7d1883a372e6fb2c490f5066
ayb1337 <aero.glowsky@gmail.com>
2023-07-25 23:37:40 +0100

    all

--- README.md
+++ README.md
@@ -1,10 +1,1 @@
-# 0x11. C - printf
-
-
- ### This is the printf team project 
-
-
-
-
-0. I'm not going anywhere. You can print that wherever you want to. I'm here and I'm a Spur for life
-1. Education is when you read the fine print. Experience is what you get if you don't
+### PRINTF 
--- _putchar.c
+++ _putchar.c
@@ -1,13 +1,10 @@
-#include "main.h"
 #include <unistd.h>
 /**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
+ * _putchar - write character c to the stdout
+ * @c: the character
+ * Return: 1 on success and -1 otherwise
  */
 int _putchar(char c)
 {
-return (write(1, &c, 1));
+	return (write(1, &c, 1));
 }
